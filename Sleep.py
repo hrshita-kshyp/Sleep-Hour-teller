2da5d31e60875d1716ad298bd094376830daffcd
Harshita <85073963+hrshita-kshyp@users.noreply.github.com>
2021-06-26 15:07:47 +0530

    Update Sleep.py

--- Sleep.py
+++ Sleep.py
@@ -12,7 +12,8 @@ class Sleep:
         start_time = int(input("Enter when you slept(in hours):"))
         end_time = int(input("Enter when you wake up(in hours):"))
         sleep_total = start_time - end_time
-        print(f"You slept for {sleep_total}")
+        
+        print(f"You slept for {abs(sleep_total)}")
         if sleep_total > 8:
           print("You slept more than enough today")
         elif sleep_total == 8:
