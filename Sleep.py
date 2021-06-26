688141c555335e81bb998acc0c4b689bd7c4b658
Harshita <85073963+hrshita-kshyp@users.noreply.github.com>
2021-06-26 14:55:54 +0530

    Update Sleep.py

--- Sleep.py
+++ Sleep.py
@@ -1,19 +1,27 @@
+print("******************************************************************************")
+print("WELCOME TO SLEEP HOUR TELLER")
+print("*******************************************************************************")
+
+
+
 class Sleep:
-     def __init__(self,name,sleep_time):
-        self.name = name
-        self.sleep_time = sleep_time
+     @classmethod   
+     def sleep_time(self):
+        name = input("Enter your name:")
+        print("Hi" '\t' + name + "!")
+        start_time = int(input("Enter when you slept(in hours):"))
+        end_time = int(input("Enter when you wake up(in hours):"))
+        sleep_total = start_time - end_time
+        print(f"You slept for {sleep_total}")
+        if sleep_total > 8:
+          print("You slept more than enough today")
+        elif sleep_total == 8:
+          print("You slept enough")
+        else:
+          print("You need more sleep")
+    
+             
+Sleep.sleep_time()
 
-     @classmethod
-     def cal_sleep(self):
-       name = input("Enter your name:")
-       print("Hi" + name + "!")
-       sleep_time = int(input("Enter your sleep time:"))
-       if sleep_time >8:
-            print("You slept more than enough today")
-       else:
-              print("You need some sleep")
 
 
-
-a = Sleep.cal_sleep()
-Sleep.cal_sleep()
